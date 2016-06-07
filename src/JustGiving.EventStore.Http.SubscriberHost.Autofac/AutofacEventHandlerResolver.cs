a00b272801d26bb7df7552a766bfd0a60c8fbed6
James Hulse <james.hulse@justgiving.com>
2016-06-07 15:43:58 +0100

    Added autofac IEventHandlerResolver

---
+++ src/JustGiving.EventStore.Http.SubscriberHost.Autofac/AutofacEventHandlerResolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace JustGiving.EventStore.Http.SubscriberHost.Autofac
+{
+    public class AutofacEventHandlerResolver : IEventHandlerResolver
+    {
+        private readonly ILifetimeScope _scope;
+
+        public AutofacEventHandlerResolver(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+
+        public IEnumerable GetHandlersOf(Type handlerType)
+        {
+            var resolveType = typeof (IEnumerable<>).MakeGenericType(handlerType);
+
+            return (IEnumerable) _scope.Resolve(resolveType);
+        }
+    }
+}
